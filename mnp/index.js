17e2385835fab2202e52b18218ef3c6ce2b4cb09
Anton <anton@adc.sh>
2020-01-17 12:30:41 +0300

    env

--- mnp/index.js
+++ mnp/index.js
@@ -1,5 +1,3 @@
-import { readFileSync, writeFileSync } from 'fs'
-
 /**
  * @type {Template}
  */
@@ -37,7 +35,7 @@ const config = {
       getDefault ({ name, org }) {
         return `${org}.github.io/${name}`
       },
-      afterQuestions({ org, name }, staticHost) {
+      afterQuestions({ org, name, writeFileSync }, staticHost) {
         const def = `${org}.github.io/${name}`
         if (def != staticHost) {
           writeFileSync('docs/CNAME', staticHost)
@@ -45,18 +43,20 @@ const config = {
       },
     },
   },
-  async afterInit({ org, name }, { git, loading, github, initManager }) {
+  async afterInit({ org, name }, { git, loading, github, initManager, updateFiles }) {
     await initManager()
     const { generateVAPIDKeys } = require('web-push')
     const { sync } = require('uid-safe')
     const { publicKey, privateKey } = generateVAPIDKeys()
-    let env = readFileSync('.env', 'utf8')
-    env = `\nPUBLIC_VAPID=${publicKey}\nPRIVATE_VAPID=${privateKey}`
+    let env = `\nPUBLIC_VAPID=${publicKey}\nPRIVATE_VAPID=${privateKey}`
     const session = sync(18)
     const captcha = sync(18)
     env = env.replace('SESSION_KEY=001101', `SESSION_KEY=${session}`)
     env = env.replace('CAPTCHA_KEY=catpcha', `CAPTCHA_KEY=${captcha}`)
-    writeFileSync('.env', env)
+    await updateFiles({
+      re: /$/,
+      replacement: env,
+    }, { file: '.env' })
     await loading('Enabling Pages on docs', github.pages.enable(org, name))
     await git(['rm', '--cached', '.env', '.settings'])
     await git(['commit', '-am', 'ignore settings'])
